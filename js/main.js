72f46c59142eb220f26ace52568acec64aaeeae9
mzz2017 <m@mzz.pub>
2019-04-28 21:39:35 +0800

    fix/style: 修复linux无法正常使用的问题，美化样式

--- js/main.js
+++ js/main.js
@@ -1,123 +1,134 @@
+$(function () {
+    const keys = ['ak', 'sk', 'bucket', 'domain'];
+    let isConfig = true;
 
-$(function() {
-  const keys = ['ak', 'sk', 'bucket', 'domain'];
-  let isConfig = true;
-  
-  keys.forEach(key => {
-    if (!localStorage[key]) {
-      isConfig = false;
-    }
-  })
-
-  if (!isConfig) {
-    chrome.tabs.create({url: 'options.html'});
-  }
-  const uptoken = genUpToken(localStorage['ak'], localStorage['sk'], localStorage['bucket']);
-  const domain = localStorage['domain'].indexOf('http') == -1 ? 'http://' + localStorage['domain'] : localStorage['domain'];
-  var uploader = Qiniu.uploader({
-    disable_statistics_report: false,
-    runtimes: 'html5,flash,html4',
-    browse_button: 'pickfiles',
-    container: 'container',
-    drop_element: 'container',
-    max_file_size: '1000mb',
-    flash_swf_url: 'bower_components/plupload/js/Moxie.swf',
-    dragdrop: true,
-    chunk_size: '4mb',
-    multi_selection: !(moxie.core.utils.Env.OS.toLowerCase() === "ios"),
-    uptoken: uptoken,
-    domain: 'https://upload.qbox.me',
-    get_new_uptoken: false,
-    auto_start: true,
-    log_level: 5,
-    init: {
-      'BeforeChunkUpload': function(up, file) {
-        console.log("before chunk upload:", file.name);
-      },
-      'FilesAdded': function(up, files) {
-        $('.words').html('上传中...');
-      },
-      'BeforeUpload': function(up, file) {
-        console.log("this is a beforeupload function from init");
-        var chunk_size = plupload.parseSize(this.getOption(
-          'chunk_size'));
-      },
-      'UploadProgress': function(up, file) {
-      },
-      'UploadComplete': function() {
-        $('.words').html('上传完成');
-        $('.status').css('display', 'flex');
-        setTimeout(() => {
-          $('.status').css('display', 'none');
-        }, 3000);
-        $('#link-area').css('visibility', 'visible');
-
-      },
-      'FileUploaded': function(up, file, info) {
-        console.log("response:", info.response);
-        res = info.response;
-        res = JSON.parse(res);
-        const link = domain + '/' + res.key;
-        const markdownLink = '![' + res.key + '](' + link + ')';
-        const aLink = '<a href="' + link + '"><img src="' + link + '" alt="' + res.key + '"></a>';
-        $('#link').val(link);
-        $('#markdown').val(markdownLink);
-        $('#a-link').val(aLink);
-      },
-      'Error': function(up, err, errTip) {
+    keys.forEach(key => {
+        if (!localStorage[key]) {
+            isConfig = false;
         }
+    })
+
+    if (!isConfig) {
+        chrome.tabs.create({url: 'options.html'});
     }
-  });
+    const uptoken = genUpToken(localStorage['ak'], localStorage['sk'], localStorage['bucket']);
+    const domain = localStorage['domain'].indexOf('http') == -1 ? 'http://' + localStorage['domain'] : localStorage['domain'];
+    var uploader = Qiniu.uploader({
+        disable_statistics_report: false,
+        runtimes: 'html5,flash,html4',
+        browse_button: 'pickfiles',
+        container: 'container',
+        drop_element: 'container',
+        max_file_size: '1000mb',
+        flash_swf_url: 'bower_components/plupload/js/Moxie.swf',
+        dragdrop: true,
+        chunk_size: '4mb',
+        multi_selection: !(moxie.core.utils.Env.OS.toLowerCase() === "ios"),
+        uptoken: uptoken,
+        domain: 'https://upload.qbox.me',
+        get_new_uptoken: false,
+        auto_start: true,
+        log_level: 5,
+        init: {
+            'BeforeChunkUpload': function (up, file) {
+                console.log("before chunk upload:", file.name);
+            },
+            'FilesAdded': function (up, files) {
+                $('.words').html('上传中...');
+            },
+            'BeforeUpload': function (up, file) {
+                console.log("this is a beforeupload function from init");
+                var chunk_size = plupload.parseSize(this.getOption(
+                    'chunk_size'));
+            },
+            'UploadProgress': function (up, file) {
+            },
+            'UploadComplete': function () {
+                $('.words').html('上传完成');
+                $('.status').css('display', 'flex');
+                setTimeout(() => {
+                    $('.status').css('display', 'none');
+                }, 3000);
+                $('#link-area').css('visibility', 'visible');
 
-  $('#container').on(
-    'dragenter',
-    function(e) {
-      e.preventDefault();
-      $('#container').addClass('draging');
-      e.stopPropagation();
+            },
+            'FileUploaded': function (up, file, info) {
+                console.log("response:", info.response);
+                res = info.response;
+                res = JSON.parse(res);
+                const link = domain + '/' + res.key;
+                const markdownLink = '![' + res.key + '](' + link + ')';
+                const aLink = '<a href="' + link + '"><img src="' + link + '" alt="' + res.key + '"></a>';
+                $('#link').val(link);
+                $('#markdown').val(markdownLink);
+                $('#a-link').val(aLink);
+            },
+            'Error': function (up, err, errTip) {
+                if (errTip.indexOf('401') !== -1) {
+                    errTip = "鉴权失败，请确定AK/SK配置正确"
+                }
+                $('.words').html(`上传失败: ${errTip}`);
+                $('.status').css('display', 'flex');
+                $('.status img').css('display', 'none');
+                setTimeout(() => {
+                    $('.status').css('display', 'none');
+                    $('.status img').css('display', 'unset');
+                }, 5000);
+                $('#link-area').css('visibility', 'visible');
+            }
+        }
+    });
+
+    $('#container').on(
+        'dragenter',
+        function (e) {
+            e.preventDefault();
+            $('#container').addClass('draging');
+            e.stopPropagation();
+        }
+    ).on('drop', function (e) {
+        e.preventDefault();
+        $('#container').removeClass('draging');
+        e.stopPropagation();
+    }).on('dragleave', function (e) {
+        e.preventDefault();
+        $('#container').removeClass('draging');
+        e.stopPropagation();
+    }).on('dragover', function (e) {
+        e.preventDefault();
+        $('#container').addClass('draging');
+        e.stopPropagation();
+    });
+
+    $('#link-area button[data-action=copy]').on('click', function (e) {
+        const btn = e.target;
+        const targetId = $(btn).attr('data-action-target');
+        const val = $(targetId).val();
+        copyToClipboard(val);
+    });
+
+    function copyToClipboard(input) {
+        const el = document.createElement('textarea');
+        el.style.fontsize = '12pt'
+        el.style.border = '0'
+        el.style.padding = '0'
+        el.style.margin = '0'
+        el.style.position = 'absolute'
+        el.style.left = '-9999px'
+        el.setAttribute('readonly', '')
+        el.value = input
+
+        document.body.appendChild(el)
+        el.select()
+
+        let success = false;
+        try {
+            success = document.execCommand('copy', true);
+        } catch (err) {
+        }
+
+        document.body.removeChild(el);
+
+        return success;
     }
-  ).on('drop', function(e) {
-    e.preventDefault();
-    $('#container').removeClass('draging');
-    e.stopPropagation();
-  }).on('dragleave', function(e) {
-    e.preventDefault();
-    $('#container').removeClass('draging');
-    e.stopPropagation();
-  }).on('dragover', function(e) {
-    e.preventDefault();
-    $('#container').addClass('draging');
-    e.stopPropagation();
-  });
-
-  $('#link-area').on('click', function(e) {
-    const btn = e.target;
-    const targetId = $(btn).attr('data-action-target');
-    const val = $(targetId).val();
-    copyToClipboard(val);
-  });
-
-  function copyToClipboard(input) {
-    const el = document.createElement('textarea');
-    el.style.fontsize = '12pt'
-    el.style.border = '0'
-    el.style.padding = '0'
-    el.style.margin = '0'
-    el.style.position = 'absolute'
-    el.style.left = '-9999px'
-    el.setAttribute('readonly', '')
-    el.value = input
-
-    document.body.appendChild(el)
-    el.select()
-
-    let success = false;
-    try {
-      success = document.execCommand('copy', true);
-    } catch (err) { }
-
-    document.body.removeChild(el);
-
-    return success;
-  }
 });
--- js/options.js
+++ js/options.js
@@ -1,40 +1,43 @@
 function Options() {
-  const fields = ['ak', 'sk', 'bucket', 'domain'];
-  let isConfig = true;
+    const fields = ['ak', 'sk', 'bucket', 'domain'];
 
-  var bindUI = function () {
-    fields.forEach(item => {
-      document.querySelector('#' + item).value = localStorage[item] ? localStorage[item] : '';
-    })
+    var bindUI = function () {
+        //用localstorage中的值填充每个fields
+        fields.forEach(field => {
+            const ls = localStorage.getItem(field)
+            document.querySelector(`#${field}`).value = ls ? ls : '';
+        });
 
-    document.querySelector('.save').addEventListener('click', e => {
-      fields.forEach(item => {
-        const val = document.querySelector('#' + item).value.trim();
-        if (val) {
-          localStorage[item] = val;
+        //监听save按钮的click事件
+        document.querySelector('.save').addEventListener('click', e => {
+            fields.forEach(field => {
+                const val = document.querySelector(`#${field}`).value.trim();
+                if (!val) {
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: "image.png",
+                        title: "提示",
+                        message: "为了图床的正常使用，请不要留空",
+                        requireInteraction: true,
+                    });
+                    return
+                }
+                localStorage.setItem(field, val);
+            });
+            document.querySelector('.save').innerText = '保存成功';
+            setTimeout(() => {
+                chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+                    chrome.tabs.remove(tabs[0].id);
+                });
+            }, 500)
+        })
+    };
+
+    return {
+        init: function () {
+            bindUI();
         }
-      })
-      fields.forEach(key => {
-        if (!localStorage[key]) {
-          isConfig = false;
-        }
-      })
-      if (isConfig) {
-        document.querySelector('.save').innerText = '保存成功';
-        setTimeout(() => {
-          chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-            chrome.tabs.remove(tabs[0].id);
-          })
-        }, 1500)
-      }
-    })
-  };
-
-  return {
-      init: function () {
-          bindUI();
-      }
-  };
+    };
 }
 
 const options = new Options();
